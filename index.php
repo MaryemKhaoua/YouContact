f043ef13a2691007e3206611873c28b06eb869a2
Maryem Khaoua <maryem.khaoua@gmail.com>
2023-11-22 23:33:17 +0100

    connect the data base

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include_once 'includes/dbh.inc.php'
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YouConact</title>
+    <!-- link bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    <!-- link css -->
+	
+</head>
+<body>
+    <main> 
+        <header>
+  <!-- nav bar -->
+  <nav class="nav">
+    <a class="nav-link active" aria-current="page"  href="#">YouConact</a>
+        <button  class="btn btn-outline-success" type="submit">connexion</button>
+  </nav>
+        </header>
+      
+    </main>
+</body>
+</html>
