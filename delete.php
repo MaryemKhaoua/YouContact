4b18f923ff9128a9f2b47725c237510d34b2e5de
Maryem Khaoua <maryem.khaoua@gmail.com>
2023-11-27 17:18:26 +0100

    modify delete

--- delete.php
+++ delete.php
@@ -1,13 +1,10 @@
 <?php 
-include './connexion.php';
+include 'connexion.php';
 
 
 $id = $_GET['id']; 
-
-$sql = "DELETE FROM contacts WHERE id_contact = $id";
+$sql = "DELETE FROM contacts WHERE id_contact ='$id'";
 mysqli_query($conn, $sql);
-
-
 
 header('location: index.php'); 
 
